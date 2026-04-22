2fe323fa288b2c9516c96ba8126812191ac5b567
Jecu Mihaela - Birou <mihaela.nicoleta.jecu@gmail.com>
2026-04-22 16:49:49 +0300

    Add service class

--- src/main/java/station/disponibility/cron/job/service/StationDisponibilityService.java
+++ src/main/java/station/disponibility/cron/job/service/StationDisponibilityService.java
@@ -5,13 +5,11 @@ import com.jcraft.jsch.Session;
 import com.jcraft.jsch.SftpException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import station.disponibility.cron.job.config.SshConfig;
 import station.disponibility.cron.job.config.SshResourcesConfig;
 import station.disponibility.cron.job.dto.StationDto;
 import station.disponibility.cron.job.dto.Stations;
 
 import java.time.LocalDate;
-import java.util.spi.LocaleServiceProvider;
 
 @Service
 @AllArgsConstructor
@@ -23,21 +21,21 @@ public class StationDisponibilityService {
             String stationPath = getStationPath(station.name());
             System.out.println(stationPath);
 
-            System.out.println( fileExists(session, stationPath));
+            System.out.println(fileExists(session, stationPath));
         }
     }
 
-    private String getStationPath(String stationName){
+    private String getStationPath(String stationName) {
         StationDto stationDto = new StationDto(LocalDate.now());
         String station = stationName.concat("00ROU_R_").concat(stationDto.getYear().toString())
-//                .concat(String.format("%03d",stationDto.getDay()))
                 .concat(String.format("%03d",stationDto.getDay()))
-                .concat("0000_01D_30S_MO.");
-      return  sshResourcesConfig.getPath().concat(stationDto.getYear().toString()).concat("/")
-                .concat(String.format("%03d",stationDto.getDay())).concat("/")
+                .concat("0000_01D_30S_MO.crx.gz");
+        return sshResourcesConfig.getPath().concat(stationDto.getYear().toString()).concat("/")
+                .concat(String.format("%03d", stationDto.getDay()))
+                .concat("/")
                 .concat(station);
-      //ARYB00ROU_R_20260010000_01D_30S_MO.crx.gz
-      // ARYB00ROU_R_20260010000_01D_30S_MO.rnx.gz
+        //ARYB00ROU_R_20260010000_01D_30S_MO.crx.gz
+        // ARYB00ROU_R_20260010000_01D_30S_MO.rnx.gz
     }
 
     public boolean fileExists(Session session, String path) {
